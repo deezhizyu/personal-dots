ac22cd239cd806c8221f0d8e944db5ac7b66be8c
deezhizyu <kalenskuja@gmail.com>
2025-05-18 11:59:20 +0200

    Update fish config

--- .config/fish/conf.d/PATH.fish
+++
@@ -1,3 +0,0 @@
-# Custom PATH environment
-fish_add_path /home/bypass/.local/bin
-fish_add_path /opt/android-sdk/platform-tools
--- .config/fish/conf.d/init.fish
+++
@@ -1,5 +0,0 @@
-# Initialize software
-zoxide init fish --cmd cd | source
-thefuck --alias | source
-fzf --fish | source
-starship init fish | source
