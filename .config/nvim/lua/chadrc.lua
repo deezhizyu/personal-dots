a49ba5a8e7d0880251eb5200fd22efdfa28561fa
deezhizyu <kalenskuja@gmail.com>
2025-05-24 14:59:38 +0200

    Fix nvim theme and add weather to waybar

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,41 +1,24 @@
 -- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "catppuccin",
+	theme = "tomorrow_night",
 
-	hl_override = {
-		Comment = { italic = true },
-		["@comment"] = { italic = true },
-	},
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
 }
-M.ui = {
-	theme = 'default',
-	transparency = true,
-	cmp = {
-    lspkind_text = true,
-    icons_left = true,
-    style = "flat_dark",
-  },
-  statusline = {
-		theme = 'minimal',
-		separator_style = 'block'
-  },
-}
-M.nvdash = {
-		load_on_startup = true,
-    header = {
-      [[ ▄▄▄▄·  ▄· ▄▌ ▄▄▄· ▄▄▄· .▄▄ · .▄▄ ·  ]],
-      [[ ▐█ ▀█▪▐█▪██▌▐█ ▄█▐█ ▀█ ▐█ ▀. ▐█ ▀.  ]],
-      [[ ▐█▀▀█▄▐█▌▐█▪ ██▀·▄█▀▀█ ▄▀▀▀█▄▄▀▀▀█▄ ]],
-      [[ ██▄▪▐█ ▐█▀·.▐█▪·•▐█ ▪▐▌▐█▄▪▐█▐█▄▪▐█ ]],
-      [[ ·▀▀▀▀   ▀ • .▀    ▀  ▀  ▀▀▀▀  ▀▀▀▀  ]],
-      [[ ]],
-      [[ ]]
-    }
-}
+
+-- M.nvdash = { load_on_startup = true }
+-- M.ui = {
+--       tabufline = {
+--          lazyload = false
+--      }
+--}
 
 return M
